1b214f9e810f1ed892a0f04e45a799d77953b34b
RedDusty <zprobius@gmail.com>
2021-02-20 09:27:04 +0300

    redux pkg add

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, Provider } from "redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
